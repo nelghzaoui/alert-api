b1015b264d0070f8728ec857e91298999dcb4d02
Nasreddine <nasreddine@MacBook-Pro-de-Nasreddine.local>
2020-10-22 01:19:49 +0200

    refactor app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
 
-/* Custom Module */
 import { AccountModule } from '@account/account.module';
 import { AlertModule } from '@alert/alert.module';
 import { CoreModule } from '@core/core.module';
 import { ServerModule } from '@server/server.module';
-import { GraphQLModule } from '@nestjs/graphql';
 
 const databaseUrl =
   'mongodb+srv://nasreddine:G5iECWApFc7JQmHF@cluster0.yejys.gcp.mongodb.net/alert?retryWrites=true&w=majority';
 @Module({
   imports: [
-    AccountModule,
-    AlertModule,
-    CoreModule,
-    ServerModule,
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
     }),
     MongooseModule.forRoot(databaseUrl),
+    AccountModule,
+    AlertModule,
+    CoreModule,
+    ServerModule,
   ],
 })
 export class AppModule {}

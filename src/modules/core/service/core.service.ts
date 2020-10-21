dd6957def01780c8fba1d3a7ec01e243e90a3ea3
Nasreddine <eln@outlook.be>
2020-10-22 01:33:26 +0200

    remove unused function

--- src/modules/core/service/core.service.ts
+++ src/modules/core/service/core.service.ts
@@ -1,30 +1,6 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CoreService {
   constructor() {}
-
-  // public async _delete(model: Model<any, {}>, id: string): Promise<void> {
-  //   const result = await model.deleteOne({ _id: id }).exec();
-
-  //   if (result.n === 0) {
-  //     throw new NotFoundException('Could not find server.');
-  //   }
-  // }
-
-  protected async find<T>(model: Model<any, {}>, id: string): Promise<T> {
-    let _model: any;
-    try {
-      _model = await model.findById(id).exec();
-    } catch (error) {
-      throw new NotFoundException('Could not find account.');
-    }
-
-    if (!_model) {
-      throw new NotFoundException('Could not find account.');
-    }
-
-    return _model;
-  }
 }
